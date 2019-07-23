08063bbf83487140f7b25d9142e7b42c1f0653f2
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-23 10:28:18 +0300

    Adding Auto Login

--- src/store.js
+++ src/store.js
@@ -43,6 +43,13 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           });
+
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000);
+          localStorage.setItem('token', res.data.idToken);
+          localStorage.setItem('userId', res.data.localId);
+          localStorage.setItem('expirationDate', expirationDate);
+
           dispatch('storeUser', payload);
           dispatch('setLogoutTimer', res.data.expiresIn);
           router.replace('/dashboard');
@@ -61,14 +68,44 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           });
+
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000);
+          localStorage.setItem('token', res.data.idToken);
+          localStorage.setItem('userId', res.data.localId);
+          localStorage.setItem('expirationDate', expirationDate);
           dispatch('setLogoutTimer', res.data.expiresIn);
+
           router.replace('/dashboard');
         })
         .catch(error => console.log(error))
     },
+    tryToAutoLogin({ commit }) {
+
+      const token = localStorage.getItem('token');
+      if (!token) {
+        return
+      }
+
+      const expirationDate = localStorage.getItem('expirationDate');
+      const now = new Date();
+      if (now <= expirationDate) {
+        return
+      }
+
+      const userId = localStorage.getItem('userId');
+      commit('authUser', {
+        token: token,
+        userId: userId
+      })
+
+    },
     logout({ commit }) {
 
       commit('clearAuthData');
+      localStorage.removeItem('expirationDate');
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
       router.replace('/signin');
     },
     storeUser({ commit, state }, payload) {
